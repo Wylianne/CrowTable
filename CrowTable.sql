2d5a5323dd6c7743e198731c5b67f9c72f73433c
Victor Cardoso <victor.eacd@gmail.com>
2016-10-30 19:48:15 -0300

    Tabela sql

---
+++ CrowTable.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Community v12.09 (32 bit)
+MySQL - 5.5.49-0ubuntu0.14.04.1 : Database - CrowTable
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`CrowTable` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `CrowTable`;
+
+/*Table structure for table `cliente` */
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(20) DEFAULT NULL,
+  `nome` varchar(220) DEFAULT NULL,
+  `identidade` varchar(30) DEFAULT NULL,
+  `dataNascimento` varchar(10) DEFAULT NULL,
+  `escolaridade` varchar(30) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `cliente` */
+
+LOCK TABLES `cliente` WRITE;
+
+insert  into `cliente`(`id`,`cpf`,`nome`,`identidade`,`dataNascimento`,`escolaridade`,`endereco`,`telefone`) values (1,'09990990902','victor','222333444','22/12/3213','fundamental','rua santo amaro','9009-0909');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
